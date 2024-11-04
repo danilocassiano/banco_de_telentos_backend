c90dc2a17779efce04bfa9ad6032d2638274ccd3
Alison Simião <alison.s@multiplier.com.br>
2024-11-04 00:17:35 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(
     cors({
-      origin: 'http://localhost:5173', 
+      origin: '*',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
     }),
